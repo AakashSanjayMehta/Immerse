e4b3d94ee9fff3cf91fc71654be39ee31269f763
benwoo1110 <benedict_woo_jun_kai@s2016.sst.edu.sg>
2018-07-08 21:44:51 +0800

    Edited MainVC button. Edited text on 4 webpage buttons on SelectionVC. Minor constrains adjustments.

--- Immerse/AppDelegate.swift
+++ Immerse/AppDelegate.swift
@@ -44,6 +44,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Saves changes in the application's managed object context before the application terminates.
         self.saveContext()
     }
+    
+    /// set orientations you want to be allowed in this property by default
+    var orientationLock = UIInterfaceOrientationMask.all
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return self.orientationLock
+    }
 
     // MARK: - Core Data stack
 
