43f8589160a6df938ee8b4d702cc42839eb3a6db
AakashSanjayMehta <asrm003@gmail.com>
2018-11-12 15:15:07 +0800

    PVC #1

---
+++ Immerse/CCAPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CCAViewController.swift
+//  Immerse
+//
+//  Created by Aakash Sanjay Mehta on 12/11/18.
+//  Copyright © 2018 Aakash Sanjay Mehta. All rights reserved.
+//
+
+import UIKit
+
+class CCAPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
